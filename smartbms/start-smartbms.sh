4c19e1137b78e554e81a0f9ae8298c68f83c2496
AlbertronicDev <development@albertronic.com>
2021-09-02 16:06:24 +0200

    First version of SmartBMS to Venus

---
+++ smartbms/start-smartbms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Start script for BMV vedirect
+#	First parameter: tty device to use
+#
+# Keep this script running with daemon tools. If it exits because the
+# connection crashes, or whatever, daemon tools will start a new one.
+#
+
+. /opt/victronenergy/serial-starter/run-service.sh $tty
+
+if [ ! -h $alias ]; then ln -s /dev/$tty $alias; fi
+app="/usr/bin/python /data/smartbms/smartbms.py"
+start -d /dev/$tty
